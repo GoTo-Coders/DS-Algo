337a55476fd76972dc51cff4db1fbacb3e57e5f6
ankit-sr <ankitrajput6622@gmail.com>
2021-07-15 18:32:16 +0530

    Changed approach from procedural to object oriented

--- C Plus Plus/Graph/Breadth_First_Search.cpp
+++ C Plus Plus/Graph/Breadth_First_Search.cpp
@@ -1,63 +1,99 @@
 /*
-   Breadth first search is a graph traversal algorithm that starts traversing the graph from root node and explores all the neighbouring nodes. 
-   Then, it selects the nearest node and explore all the unexplored nodes. 
-   The algorithm follows the same process for each of the nearest node until it finds the goal.
+    Breadth First Search Traversal is a popular traversal method in graph.
+    It uses queue data structure for storing elements before output.
+    The pseudo code for the traversal method is as follows:
+        1. Initialize a boolean vecotr visited of size of number of vertices with false,
+           which indicates whether a vertex is already visited or not.
+        2. Push the source (generally 0 vertex) in the queue and set the visited[vertex] to true.
+        3. Pop the front node.
+            a. Print data.
+            b. Find all the adjacent vertices with visited status false.
+            c. Push them in the queue and set the visited status to true.
+        4. Repeat step 3 until queue becomes empty.
+        5. Finish and Return.
 */
 
-
-#include <iostream>
-#include <vector>
-#include <queue>
+// Implementation
+#include<bits/stdc++.h>
 using namespace std;
 
-void addEdge(vector<int> graph[], int u, int v) {
-    graph[u].emplace_back(v);
-    graph[v].emplace_back(u);
+// Structure of Graph
+class Graph{
+
+    // Number of vertices
+    int vertices;
+
+    // Will contain the vertices which are adjacent to other.
+    vector<int> *adjacency;
+
+    public:
+        // Constructor
+        Graph(int vertices){
+            this->vertices = vertices;
+            
+            // Creating a array of  vector to store the adajacent vertices with
+            // respect to the index of source vertex.
+            adjacency = new vector<int>[vertices];
+            
+        }
+
+        // Method to add a directed edge bvetween two vertices.
+        void addedge(int , int);
+
+        // Method to print the vertex of the graph in Breadth First Search pattern.
+        void breadth_first_search();
+};
+
+void Graph::addedge(int source, int destination){
+    adjacency[source].emplace_back(destination);
 }
 
-void bfs(vector<int> graph[], int start)
-{
-    vector<bool> visited(graph->size(), false);
+
+void Graph::breadth_first_search(){
+    // Vector to contain visited status of vertices, initialized with false values.
+    vector<bool> visited(adjacency->size(), false);
+
     queue<int> q;
-    q.push(start);
-    visited[start] = true;
-    while (!q.empty()) {
-        int v = q.front();
-        cout << v << "  ";
+    q.push(0);
+    visited[0] = true;
+
+    // Continue loop, until queue becomes empty.
+    while(!q.empty()){
+        int vertex = q.front();
+        cout<< "V" << vertex <<" ";
         q.pop();
-        // Insert all adjacent nodes of v and mark them visited to avoid cycles.
-        for (auto i = graph[v].begin(); i != graph[v].end(); i++) {
-            if (!visited[*i]) {
+
+        // Find all the adjacent unvisited vertices and push the in the queue.
+        for(auto i = adjacency[vertex].begin(); i != adjacency[vertex].end(); i++){
+            if(!visited[*i]){
                 q.push(*i);
                 visited[*i] = true;
             }
         }
     }
 }
-int main() {
-    int V = 9;
-    vector<int> graph[V];
+
+
+int main(){
+    // Object of graph with 5 vertices.
+    Graph graph(5);
+
+    // Adding edges to the graph.
+    graph.addedge(0, 1);
+    graph.addedge(0, 2);
+    graph.addedge(1, 2);
+    graph.addedge(1, 4);
+    graph.addedge(1, 3);
+    graph.addedge(2, 4);
+    graph.addedge(3, 4);
+
+    // Printing the vertices in Breadth First Search manner.
+    cout<<"Breadth First Search Traversal : ";
+    graph.breadth_first_search();
     
-    //Pair of Nodes
-    addEdge(graph, 0, 1);
-    addEdge(graph, 0, 3);
-    addEdge(graph, 1, 2);
-    addEdge(graph, 2, 3);
-    addEdge(graph, 2, 5);
-    addEdge(graph, 3, 4);
-    addEdge(graph, 4, 5);
-    addEdge(graph, 4, 6);
-    addEdge(graph, 5, 7);
-    addEdge(graph, 6, 7);
-
-    //Print BFS Traversal
-    cout << "BFS traversal:" << "  ";
-    bfs(graph, 0);
-}
-
-
+    
 /*
-    Time complexity - O(V + E)
-    Space complexity - O(|V|) 
-    Where E is number of edges and V is number of vertices.
-*/  
+    Time Complexity - O(|V| + |E|)
+    Space Complexity - O(|V|)
+    where V is the number of vertices and E is the number of edges present in the graph.
+*/
